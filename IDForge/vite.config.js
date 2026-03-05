670618114a59bd60699018e837cb13595ab5da3f
Pavan Ch <52743793+chpavan642@users.noreply.github.com>
2026-03-05 18:55:41 +0000

    deploy erroe

--- IDForge/vite.config.js
+++ IDForge/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Replace 'idcard-app' with YOUR actual GitHub repo name
+  base: '/IDForge/',
   server: {
     host: '0.0.0.0',
     port: 5173,
